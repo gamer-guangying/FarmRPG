e41c91ae7892a34248c35fe0e89235f5665a7910
gqyyyyy <3605625323@qq.com>
2025-05-21 23:46:27 +0800

    设置了一下场景
    加了空对象UIManager以及canvas和EventSystem

--- Assets/Scripts/PlantingSystem/PlantingManager.cs
+++ Assets/Scripts/PlantingSystem/PlantingManager.cs
@@ -1,13 +1,14 @@
 // PlantingManager.cs
 using UnityEngine;
-
+using System.Collections.Generic;
 public class PlantingManager : MonoBehaviour
 {
     [Tooltip("当前选定的作物数据")]
     public CropData selectedCrop; // 被选中的作物类型
-
+    List<FarmTile> farmTiles;
     void Update()
     {
+
         // 左键点击检测：在被点击的地块上种植作物
         if (Input.GetMouseButtonDown(0) && selectedCrop != null)
         {
